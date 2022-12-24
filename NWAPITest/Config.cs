b12a3b4bfbbb088d866c207affa1f6c6d45f4c50
Tino <36406673+Tinoyeet@users.noreply.github.com>
2022-12-24 14:10:17 +0100

    Add project files.

---
+++ NWAPITest/Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWAPITestPlugin
+{
+    public class Config
+    {
+        [Description("The Welcome message.")]
+        public string Welcome { get; set; } = "Welcome back %playername%! Don't forget to join our Discord Server.";
+        [Description("The Round End message.")]
+        public string RoundEnd { get; set; } = "Good Job %playername%!";
+        [Description("The Duartion of the Join message.")]
+        public ushort Duration { get; set; } = 5;
+        [Description("The Duartion of the Game End message.")]
+        public ushort Duration2 { get; set; } = 5;
+    }
+}
---
+++ NWAPITest/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using PluginAPI.Core.Attributes;
+using PluginAPI.Core;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWAPITestPlugin
+{
+    public class Plugin
+    {
+        [PluginEntryPoint("Welcome", "1.2.0", "Test.", "Tino")]
+        void Enabled()
+        {
+            EventManager.RegisterEvents(this);
+        }
+
+          [PluginEvent(ServerEventType.PlayerJoined)]
+          void OnPlayerJoin(Player player)
+         {
+         player.ReceiveHint(Config.Welcome.Replace("%playername%", player.Nickname), Config.Duration);
+         }
+
+        [PluginEvent(ServerEventType.RoundEnd)]
+        void OnRoundEnd(RoundSummary.LeadingTeam curLeadingTeam)
+        {
+            foreach (Player player in Server.GetPlayers())
+            {
+                Broadcast.Singleton.TargetAddElement(player.ReferenceHub.characterClassManager.connectionToClient, Config.RoundEnd.Replace("%playername%", player.Nickname), Config.Duration2, Broadcast.BroadcastFlags.Normal);
+            }
+        }
+
+        [PluginConfig]
+        public Config Config;
+    }
+}
